9b5f45010cb7af2c5376437fa7aac3d785c0c332
Dilawar Singh <dilawars@chamcham.ncbs.res.in>
2016-12-03 04:18:36 +0530

    Added some tests.

---
+++ tests/test_socket.cc
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test_socket.cc
+ *
+ *    Description:  Test the boost socket.
+ *
+ *        Version:  1.0
+ *        Created:  12/03/2016 04:17:27 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  NCBS Bangalore
+ *
+ * =====================================================================================
+ */
+
+#include "unix-server.h"
+
+int main(int argc, const char *argv[])
+{
+    
+    return 0;
+}
+
+

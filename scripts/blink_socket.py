1d759a6861f248f3ca3cd2a403898951087aaa8b
Dilawar Singh <dilawar.s.rajput@gmail.com>
2016-12-06 13:41:54 +0530

    In each tifffile,  ImageDescription tag contains field acquitition_datetime which contains a list of timstamp at which each tag was captured. Should I write it frame as well.

--- scripts/blink_socket.py
+++ scripts/blink_socket.py
@@ -8,12 +8,12 @@ does not mean the connection is terminated.
 """
 from __future__ import print_function
     
-__author__           = "Me"
-__copyright__        = "Copyright 2016, Me"
+__author__           = "Dilawar Singh"
+__copyright__        = "Copyright 2016, Dilawar Singh"
 __credits__          = ["NCBS Bangalore"]
 __license__          = "GNU GPL"
 __version__          = "1.0.0"
-__maintainer__       = "Me"
+__maintainer__       = "Dilawar Singh"
 __email__            = ""
 __status__           = "Development"
 
@@ -26,10 +26,40 @@ import errno
 import cStringIO as StringIO
 import numpy as np
 import cv2
+import threading
+import datetime
 
-
-img_shape_ = ( 1280/2, 1024/2 )
+img_shape_ = ( 1024/2, 1280/2 )
 frame_size_ = img_shape_[0] * img_shape_[1]
+
+max_frames_in_trial = 1200
+image_stack_ = None
+
+now = datetime.datetime.now().isoformat( )
+data_dir_ = 'videos_/%s' % now 
+
+metadata_ = { 'acquisition_datetime' : [ ] }
+
+if not os.path.isdir( data_dir_ ):
+    os.makedirs( data_dir_ )
+
+def save_img_stack( index ):
+    import tifffile
+    global metadata_ 
+    global image_stack_ 
+    filename = os.path.join( data_dir_, 'stack_%03d.tif' % index )
+    tifffile.imsave( filename, image_stack_, metadata = metadata_ )
+    print( '[INFO] Saved to %s' % filename )
+
+def init_stack( ):
+    global image_stack_
+    global img_shape_, max_frames_in_trial 
+    global metadata_ 
+    metadata_[ 'acquisition_datetime' ] = []
+    image_stack_ = np.zeros( 
+            shape=( max_frames_in_trial, img_shape_[0], img_shape_[1] )
+            , dtype = np.uint8
+            )
 
 sock_name_ = '/tmp/socket_blinky'
 
@@ -38,6 +68,7 @@ def poll_socket( ):
 
 def main( ):
     global img_, buf_
+    global image_stack_
     s = socket.socket( socket.AF_UNIX, socket.SOCK_STREAM )
 
     while True:
@@ -59,6 +90,9 @@ def main( ):
     totalBytesRead = 0
     totalFrames = 0
     buf = ''
+    init_stack( )
+    framesInStack = 0
+    trial_count = 0
     while True:
         try:
             data = s.recv( frame_size_ )
@@ -66,9 +100,16 @@ def main( ):
             if len( buf) >= frame_size_:
                 img = np.frombuffer( buf[:frame_size_], dtype = np.uint8 )
                 img = np.reshape( img, img_shape_ )
-                print( img.shape, img.max(), img.min(), len(img) )
-                # cv2.imshow( 'img', img )
-                # cv2.waitKey( 1 )
+                print( 'f', end='')
+                sys.stdout.flush( )
+                # print( img.shape, img.max(), img.min(), len(img) )
+                cv2.imshow( 'img', img )
+                cv2.waitKey( 1 )
+                image_stack_[ framesInStack ] = img
+                metadata_[ 'acquisition_datetime' ].append( 
+                        datetime.datetime.now().isoformat( ) 
+                                )
+                framesInStack += 1
                 buf = buf[frame_size_:]
             else:
                 pass
@@ -82,6 +123,14 @@ def main( ):
             else:
                 print('No data : %s' % e )
                 break
+
+        if framesInStack == max_frames_in_trial :
+            framesInStack = 0
+            tiff = image_stack_
+            print( "\nSize of image stack ", tiff.shape )
+            save_img_stack( trial_count )
+            trial_count += 1
+            init_stack( )
 
 if __name__ == '__main__':
     main()
